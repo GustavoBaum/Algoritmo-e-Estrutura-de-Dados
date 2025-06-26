d6e3b8c0ca4512a5a3320f4a0342be38419cf2c3
GustavoBaum <163304855+GustavoBaum@users.noreply.github.com>
2025-06-25 22:35:58 -0300

    Add files via upload

--- Ordenação/Ordenacao/src/OrdenacaoBolha.java
+++ Ordenação/Ordenacao/src/OrdenacaoBolha.java
@@ -4,10 +4,33 @@ public class OrdenacaoBolha<T extends Comparable<T>> extends OrdenacaoAbstract<T
     public void ordenar(T[] info) {
         int i, j;
         int n = info.length;
-        for(i = 0; i < n-1; i++) {
-            for (j = 0; j < i-1; j++) {
-                if (((Comparable<T>) info[i]).compareTo(info[j+1]) > 0) {
-                    trocar(info, j, j+1);
+        for (i = 0; i < n - 1; i++) {
+            for (j = 0; j < i - 1; j++) {
+                if ((info[i]).compareTo(info[j + 1]) > 0) {
+                    trocar(info, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public void ordenarDecrescente() {
+        boolean trocou = true;
+        int n = info.length;
+
+        // Continua passando enquanto houver trocas
+        while (trocou) {
+            trocou = false;
+
+            for (int i = 0; i < n - 1; i++) {
+                // Verifica se os elementos estão fora da ordem decrescente
+                if ((info[i]).compareTo(info[i + 1]) < 0) { //info[i] < info[i + 1]
+                    // Troca apenas uma vez por passagem
+                    T temp = info[i];
+                    info[i] = info[i + 1];
+                    info[i + 1] = temp;
+
+                    trocou = true;
+                    break; // Faz só uma troca por passagem
                 }
             }
         }
--- Ordenação/Ordenacao/src/TestesOrdenacao.java
+++ Ordenação/Ordenacao/src/TestesOrdenacao.java
@@ -1,14 +1,14 @@
 import java.util.Arrays;
 
 public class TestesOrdenacao {
-        
+
     public TestesOrdenacao() {
-        
+
         // @Teste1:
-        Integer[] vetor = new Integer[] {70, 2, 88, 15, 90, 30};
+        Integer[] vetor = new Integer[] { 70, 2, 88, 15, 90, 30 };
         OrdenacaoBolha<Integer> ob = new OrdenacaoBolha<>();
         ob.ordenar(vetor);
-        
+
         System.out.println("\nTeste com método Ordenação Bolha: " + Arrays.toString(vetor) + ";");
 
         // @Teste2:
@@ -17,13 +17,21 @@ public class TestesOrdenacao {
 
         System.out.println("\nTeste com método Ordenação Bolha Otimizado: " + Arrays.toString(vetor) + ";");
 
-        // @Teste3:
+        // @Teste3: 
+        OrdenacaoBolha<Integer> ordenacao = new OrdenacaoBolha<>();
+        ordenacao.setInfo(new Integer[] { 70, 2, 88, 15, 90, 30 });
+
+        ordenacao.ordenarDecrescente();
+
+        System.out.println(Arrays.toString(ordenacao.getInfo()));
+
+        // @Teste4:
         OrdenacaoQuickSort<Integer> oq = new OrdenacaoQuickSort<>();
         oq.ordenar(vetor);
 
         System.out.println("\n este com método QuickSort: " + Arrays.toString(vetor) + ";");
 
-        // @Teste4:
+        // @Teste5:
         OrdenacaoMergeSort<Integer> om = new OrdenacaoMergeSort<>();
         om.ordenar(vetor);
 
